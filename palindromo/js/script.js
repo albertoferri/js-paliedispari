86e663b35d6330161711cd7091b957694e1e4462
albertoferri <falberto99@gmail.com>
2024-02-06 16:44:55 +0100

    snack 2    inizio

---
+++ palindromo/js/script.js
@@ -0,0 +1,9 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma
+// (ad esempio, questa funzione potrebbe funzionare così: passandole la parola come parametro, 
+// farà i suoi calcoli per capire se è palindroma o meno e ci restituirà true o false in base all'esito)
+
+
+// Chiedi all'utente di inserire una parola
+let userInput = prompt("Inserisci una parola:");
