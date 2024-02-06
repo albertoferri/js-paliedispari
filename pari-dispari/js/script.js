a2590dce2e784933385124072cdc7ef83eb7466a
albertoferri <falberto99@gmail.com>
2024-02-06 15:44:10 +0100

    snack 1   inizio

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -5,3 +5,17 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function randomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function pariODispari(numero) {
+    if (numero % 2 == 0) {
+    return 'pari';
+    } else {
+    return 'dispari';
+    }
+}
