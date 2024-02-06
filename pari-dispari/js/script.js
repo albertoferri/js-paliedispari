fdf08dd21577b9e6b89f20be09617e434b986b6b
albertoferri <falberto99@gmail.com>
2024-02-06 14:10:00 +0100

    first commit

---
+++ pari-dispari/js/script.js
@@ -0,0 +1,7 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari.
+// Inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
