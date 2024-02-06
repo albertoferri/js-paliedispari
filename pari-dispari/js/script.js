72a2a99b6907a316dfbd81763e30c3afbbe6036e
albertoferri <falberto99@gmail.com>
2024-02-06 16:08:11 +0100

    snack 1      aggiunta function

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -11,7 +11,8 @@ function randomNumber() {
     return Math.floor(Math.random() * 5) + 1;
 }
 
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+// inposto funzione per capire se un numero è pari o dispari
 function pariODispari(numero) {
     if (numero % 2 == 0) {
     return 'pari';
@@ -19,3 +20,34 @@ function pariODispari(numero) {
     return 'dispari';
     }
 }
+
+document.getElementById("start").addEventListener("click", 
+    function() {
+        // inposto variabili degli elementi di input
+        let sceltaUtenteInput = document.getElementById("sceltaUtenteInput").value;
+        let numeroUtenteInput = Number(document.getElementById("numeroUtenteInput").value);
+        console.log("il tuo numero è: " + numeroUtenteInput)
+    
+        // Genera un numero casuale per il computer
+        let numeroComputer = randomNumber();
+        console.log("il n° random del computer è: " + numeroComputer)
+
+        // Sommiamo i due numeri
+        let somma = numeroUtenteInput + numeroComputer;
+
+        // Determina se la somma è pari o dispari
+        let risultato = pariODispari(somma);
+        console.log("la somma dei numeri è: " + somma + " quindi " + risultato)
+
+        // setto la variabile risposta vuota per poi inserire il testo in base alla vincita
+        let risposta = "";
+        if (risultato === sceltaUtenteInput && numeroUtenteInput >= 1 && numeroUtenteInput <= 5) {
+        risposta = "Hai vinto! Il risultato è " + risultato + ". <br> Il tuo numero: " + numeroUtenteInput + " <br> Il numero del computer: " + numeroComputer + " <br> La somma: " + somma;
+        } else {
+        risposta = "Hai perso! Il risultato è " + risultato + ". <br> Il tuo numero: " + numeroUtenteInput + " <br> Il numero del computer: " + numeroComputer + " <br> La somma: " + somma;
+        }
+    
+        // stampo in pagina la risposta
+        document.getElementById("risultato").innerHTML = risposta;
+    }
+);
